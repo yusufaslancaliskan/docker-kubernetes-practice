92d6f796c1ffae75574e52050e0896ba9a639ce8
yusufaslancaliskan <yusufaslancalskan@gmail.com>
2020-07-04 20:18:16 +0300

    docker compose

--- making-real-projects/index.js
+++ making-real-projects/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("How are you doing");
+  res.send("How are you doing Yusuf");
 });
 
 app.listen(8080, () => {
